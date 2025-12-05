5d4fd52ab447c622bf66dd87b96c63fece88c8b1
AnejVollmeier <anej.vollmeier@student.um.si>
2025-12-05 23:07:21 +0100

    Dodal sem navbar za Jedilnik,Prijavo,Registracijo,Recepte.

--- src/Frontend/Recepti/src/main.jsx
+++ src/Frontend/Recepti/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Recepti from "./pages/Recepti.jsx";
+import Jedilnik from "./pages/Jedilnik.jsx";
+import Registracija from "./pages/Registracija.jsx";
+import Prijava from "./pages/Prijava.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Recepti />} />
+        <Route path="/jedilnik" element={<Jedilnik />} />
+        <Route path="/registracija" element={<Registracija />} />
+        <Route path="/prijava" element={<Prijava />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
---
+++ src/Frontend/Recepti/src/pages/Jedilnik.jsx
@@ -0,0 +1,9 @@
+function Jedilnik() {
+  return (
+    <div>
+      <h1 className="page-title">Jedilnik</h1>
+    </div>
+  );
+}
+
+export default Jedilnik;
